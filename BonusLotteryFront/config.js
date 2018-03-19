3e496378c3f14da198182407c9c6cb751e8ef9e1
llg3footcat <279371331@qq.com>
2018-03-19 10:04:57 +0800

    足球

---
+++ BonusLotteryFront/config.js
@@ -0,0 +1,11 @@
+var engineIp = "https://mlife.cmbchina.com/";
+var debugMode = false;
+var onlyLocal = false;
+var earnUrl = "cmblife://go?url=Menu&Module=Exchange&defaultText=积分玩家";
+var engineRequestUrl = engineIp + "BonusLottery/engin/";
+var singleballRequestUrl = engineIp + "BonusLottery/";
+var soccerRequestUrl = singleballRequestUrl;
+
+//游戏内分享
+var baseServiceUrl = engineIp + "BaseService/";
+var shareUrl = engineIp + "Share/show/";
---
+++ BonusLotteryFront/game/soccer/201508252/launcher/config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by husong on 14-10-15.
+ */
+var debugMode = false;
+var onlyLocal = false;
